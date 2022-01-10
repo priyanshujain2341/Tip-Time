e52790705bd93e017eeb982b6150c07083fd96be
Priyanshu Jain <priyanshujain2341@gmail.com>
2022-01-10 19:30:06 +0530

    Add view binding

--- app/src/main/java/com/polymath/tiptime/MainActivity.kt
+++ app/src/main/java/com/polymath/tiptime/MainActivity.kt
@@ -2,10 +2,15 @@ package com.polymath.tiptime
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.polymath.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
