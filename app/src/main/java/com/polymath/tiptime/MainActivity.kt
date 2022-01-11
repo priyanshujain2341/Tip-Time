8798e144eb0cd7c006fd63d54648afb55af04785
Priyanshu Jain <priyanshujain2341@gmail.com>
2022-01-11 13:07:57 +0530

    implemented the logic to calculate the tip amount

--- app/src/main/java/com/polymath/tiptime/MainActivity.kt
+++ app/src/main/java/com/polymath/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.polymath.tiptime
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.polymath.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDouble()
+        val tipPercentageButton = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when(tipPercentageButton)
+        {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tipAmount = tipPercentage * cost
+
+        val switchValue = binding.roundTipSwitch.isChecked()
+
+        if(switchValue)
+        {
+            tipAmount = kotlin.math.ceil(tipAmount)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tipAmount)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
